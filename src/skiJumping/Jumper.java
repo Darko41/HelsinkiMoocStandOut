f63cee8deb716b0c8f72809b297c6ae368edeada
Darko41 <kracunovicd@icloud.com>
2023-11-27 22:15:01 +0100

    Create private variables in Jumper class

--- src/skiJumping/Jumper.java
+++ src/skiJumping/Jumper.java
@@ -1,5 +1,14 @@
 package skiJumping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Jumper {
+	
+	private String name;
+	private int points = 0;
+	private List<Integer> jumpLengths = new ArrayList<>();
+	private Random random = new Random();
 
 }
